0ea532be8a8ec521919adeea7eafa87c09e93a53
Jim Fisher <jameshfisher@gmail.com>
2024-07-23 14:50:40 +0100

    simplify

--- main.js
+++ main.js
@@ -1,15 +1,8 @@
 const { execSync } = require("child_process");
 const { app, BrowserWindow } = require("electron");
-const path = require("node:path");
 
 app.whenReady().then(() => {
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, "preload.js"),
-    },
-  });
+  const mainWindow = new BrowserWindow({ width: 800, height: 600 });
   mainWindow.loadFile("index.html");
 });
 
